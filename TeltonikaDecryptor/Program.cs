159e244cb199763103e9a72749ed9492e7c1c813
Andrew Shulgin <andrewshulginua@gmail.com>
2019-05-20 16:13:22 +0300

    initial commit

---
+++ TeltonikaDecryptor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Teltonika.Firmware;
+
+namespace TeltonikaDecryptor
+{
+    class Program
+    {
+        static void Usage()
+        {
+            Console.Error.WriteLine("Usage: TeltonikaDecryptor <firmware.xim> <output.bin>");
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Usage();
+                return;
+            }
+            FrameContainer container = ContainerLoader.FromFile(args[0]);
+            File.WriteAllBytes(args[1], container.Image.Array);
+        }
+    }
+}
